302cb008739aba12487569dc85ce988b9a17edf4
ZohanKPI <denizah4022@gmail.com>
2021-09-23 11:35:21 +0300

    1.0.4. Patch Notes
    Final version of the program, fixed bugs and optimized

--- third_task.py
+++ third_task.py
@@ -1,44 +1,34 @@
 def EBNF_SyntaxChecker(string):
-    result = tuple()
     if not string:
         return False, None
     elif string.isdigit():
         return True, int(string)
+
     flag = True
     subtract = False
     operationResult = 0
-    for i in range(len(string)):
-        if not i % 2:
-            if not string[i].isdigit():
-                flag = False
-                break
-            if not (string[i+1].isdigit()):
-                flag = False
-                break
-            else:
-                if not i:
-                    operationResult = int(string[i])
-                else:
-                    if subtract == False:
-                        operationResult += int(string[i])
-                    else:
-                        operationResult -= int(string[i])
-        else:
-            if string[i] not in "+-":
-                flag = False
-                break
-            else:
-                if string[i] == "+":
-                    pass
-                else:
-                    subtract = True
-    if flag == False:
-        result = (False, None)
 
-        return result
+    for i in range(0, len(string), 2):
+        if not string[i].isdigit():
+            flag = False
+            break
+        if not (string[i+1].isdigit()):
+            flag = False
+            break
+        else:
+            if not i:
+                operationResult = int(string[i])
+            else:
+                operationResult += int(string[i]) if not subtract else -int(string[i])
+        if string[i] not in "+-":
+           flag = False
+           break
     else:
-        result = (True, operationResult)
-        return result
+        if not (string[i] == "+"):
+           subtract = True
+    if not flag:
+        return False, None
+    return True, operationResult
 if __name__ == '__main__':
     string = input()
     print(EBNF_SyntaxChecker(string))
